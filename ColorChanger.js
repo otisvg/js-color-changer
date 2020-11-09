82e847aa0232d131163908960274f2d9c2ab0e1c
Otis <otisvickersgraver@gmail.com>
2020-11-09 00:37:29 +0000

    hex div changes color depending on user hex input

--- ColorChanger.js
+++ ColorChanger.js
@@ -2,16 +2,17 @@ const hexInput = document.getElementById('hexInput');
 
 const inputColor = document.getElementById('inputColor');
 
+
+
 hexInput.addEventListener('keyup', () => {
   const hex = hexInput.value;
   if(!isValidHex(hex)) return;
-
-  inputColor.style.backgroundColor = hex;
+  const strippedHex = hex.replace('#', '');
+  inputColor.style.backgroundColor = "#" + strippedHex;
 });
 
 const isValidHex = (hex) => {
   if(!hex) return false;
-
   const strippedHex = hex.replace('#', '');
   return strippedHex.length === 3 || strippedHex.length === 6;
 };
