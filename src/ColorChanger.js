3d249e018923681a16ee1a3396bc09c3003905b9
Otis <otisvickersgraver@gmail.com>
2020-11-09 00:09:55 +0000

    refactor to class structure, added jasmine testing suite to test behaviour

---
+++ src/ColorChanger.js
@@ -0,0 +1,9 @@
+class ColorChanger {
+
+  isValidHex(hex) {
+    if (!hex) return false;
+    const strippedHex = hex.replace('#', '');
+    return strippedHex.length === 3 || strippedHex.length === 6;
+  }
+
+}
